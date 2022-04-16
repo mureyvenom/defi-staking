f65c9657a5f81495065e9244b75c196128ba0924
mureyvenom <holuwamurewa@gmail.com>
2022-04-16 22:50:04 +0100

    forced workaround due to issues with web3 and react scripts

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ function App() {
     "0xb822b26fc94C745b71bF33556ED50929D4A714FC"
   );
 
-  // const loadWeb3 = async () => {
-  //   if (window.ethereum) {
-  //     window.web3 = new Web3(window.ethereum);
-  //     await window.ethereum.enable();
-  //   }
-  // };
+  const loadWeb3 = async () => {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    }
+  };
 
   return (
     <div className="text-sm">
